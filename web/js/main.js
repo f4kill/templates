e90385d8b950714d28b250749a4de646364ef655
Aymeric Bianco Pelle <biancop.aymeric@icloud.com>
2019-02-23 11:51:20 +0100

    Fixed startup code

--- web/js/main.js
+++ web/js/main.js
@@ -1,13 +1,3 @@
-let sketch = function( p ) {
-
-  p.setup = function() {
-    p.createCanvas(200, 200)
-  }
-
-  p.draw = function() {
-    p.background(0)
-
-  }
-}
-
-let myp5 = new p5(s, document.getElementById('p5sketch'))
+$(document).ready(() => {
+	
+});
